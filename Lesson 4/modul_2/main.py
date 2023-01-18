f137e99916154423d0901fb18640646af6f0f1ed
Bad_S <Evlampyev.Alex@gmail.com>
2023-01-18 23:02:17 +0500

    Lesson 4 ended all works

---
+++ Lesson 4/modul_2/main.py
@@ -0,0 +1,9 @@
+﻿import html_creater
+import xml_generator
+import data_provider as dp
+
+# print(html_creater.create())
+# print(xml_generator.create())
+
+
+html_creater.new_create(xml_generator.new_create(dp.data_collection()))
